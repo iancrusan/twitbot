d3093906ea0372c6345a91a5392899636e36e9f7
ian crusan <iancrusan@hotmail.com>
2016-08-05 11:10:16 -0400

    added post box, accepting new posts but not displaying

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,30 @@ class PagesController < ApplicationController
   def index
   end
 
+  def new
+    @post = Post.new
+
+  end
+
   def home
+    @posts = Post.all
   end
 
   def profile
-    @username = params[:id]
+    @post = Post.new
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice => "User not found"
+    end
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
   end
 
   def explore
+    @posts = Post.all
   end
+
 
 
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :content)
+
+  end
+end
